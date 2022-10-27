e1b4096912f076d1e3e3505c83ebcc2e52115c1f
沙耶 <yhsia@codersai.com>
2022-10-27 10:58:34 +0800

    Add project files.

---
+++ MauiApp1/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MauiApp1.Views;
+
+namespace MauiApp1;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+        Routing.RegisterRoute("page1", typeof(SecondPage));
+    }
+}
---
+++ MauiApp1/Views/SecondPage.xaml.cs
@@ -0,0 +1,21 @@
+namespace MauiApp1.Views;
+
+public partial class SecondPage : ContentPage
+{
+	public SecondPage()
+	{
+		InitializeComponent();
+        this.Content = new StackLayout();
+
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+    }
+}
